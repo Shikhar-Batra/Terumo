091ded952cbe47ce68359553db89a5ca345f6cef
Shikhar <shikhar@bitqit.com>
2020-02-21 17:05:42 +0530

    Initial commit

---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="homeFooterWrapper">
+        <div role="contentinfo" class="themed pageFooter ng-scope">
+            <div class="footerLinkContainer responsiveWidth  fullFooter">
+               <div class="footerLink responsiveWidth  ng-scope" ng-repeat="link in FooterLinks">
+                <a href="http://www.terumomedical.com/home/research/iisr-proposal.html" class="ng-scope ng-binding urlLink">FOR INVESTIGATORS</a>
+            </div>
+                <div class="footerLink responsiveWidth  ng-scope">
+              <a href="http://www.terumomedical.com/home/about/quality-systems.html" class="ng-scope ng-binding urlLink">QUALITY SYSTEMS</a>
+            </div>
+
+              <div class="footerLink responsiveWidth  ng-scope" >
+             <a href="http://www.terumomedical.com/home/tmc-disclaimer.html" class="ng-scope ng-binding urlLink">NOTICES</a>
+            </div>
+
+               <div class="footerLink responsiveWidth  ng-scope" >
+                <a href="http://www.terumomedical.com/home/tmc-privacy-policy.html" class="ng-scope ng-binding urlLink">PRIVACY POLICY</a>
+            </div>
+
+                <div class="footerLink responsiveWidth  ng-scope" >
+               <a href="https://www.ibm.com/privacy/details/us/en" class="ng-scope ng-binding urlLink">IBM Online Privacy Statement</a>
+            </div>
+            </div>
+        </div>
+
+    </div>
+
+</template>
+
+
+<script>
+    export default {
+        name: 'SiteFooter',
+        data () {
+            return {
+                message: 'Try change me!'
+            }
+        },
+        methods: {
+            onClick () {
+                this.message = 'Here you go :)'
+            }
+        }
+    }
+</script>
+
+<style>
+    .homeFooterWrapper .themed.pageFooter {
+        background-color: #5a5a5a;
+    }
+
+    .pageFooter {
+        left: 0;
+        bottom: 0;
+        padding: 8px;
+        height: 40px;
+        overflow: hidden;
+        width: 100%;
+
+    }
+    .pageFooter .fullFooter {
+        width: 100%;
+    }
+
+    .footerLinkContainer {
+        padding: 0;
+        display: inline-block;
+    }
+
+    .footerLink{
+        border-color: #E0E0E0;
+        color: #ffffff;
+    }
+
+
+    .pageFooter .fullFooter :first-child {
+        border-left: 0px;
+    }
+    .pageFooter .fullFooter .footerLink {
+        display: inline-block;
+    }
+    .footerLink {
+        text-decoration: none;
+        min-width: 8%;
+        max-width: 30%;
+        text-align: center;
+        border-left: 1px solid;
+        display: inline-block;
+        margin-right: 6px;
+        padding: 5px 10px;
+        -ms-text-overflow: ellipsis;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+    }
+
+
+    .pageFooter .fullFooter :first-child {
+        border-left: 0px;
+    }
+
+     .themed.pageFooter a {
+         color: #ffffff;
+         border-color: #ffffff;
+         font-size:16px !important
+     }
+    .pageFooter .urlLink {
+        border-bottom: none;
+    }
+
+
+
+</style>
+
+
---
+++ src/layouts/Default.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="layout">
+    <div class="primaryHeaderLogoWrapperLink">
+      <!--                  <img ng-if="! bNull" class="primaryHeaderLogo" alt="Terumo CV Group" dynamic-load-callback="" brand-property="primaryHeaderLogo" ng-src="https://sjobs.brassring.com/img/images_25987_5326/images/img_terumoLogo-TCG.png" src="./Job Search Welcome_files/img_terumoLogo-TCG.png">&lt;!&ndash; end ngIf: ! bNull &ndash;&gt;&lt;!&ndash; ngIf: bNull &ndash;&gt;
+      -->
+      <a href="/">
+        <g-image src="~/terumo-logo.png" alt="Terumo CV Group" class="primaryHeaderLogo"></g-image>
+      </a>&nbsp;
+    </div>
+    <header class="header">
+<!--      <strong>-->
+<!--        <g-link to="/">{{ $static.metadata.siteName }}</g-link>-->
+<!--      </strong>-->
+      <nav class="nav">
+        <g-link class="nav__link" to="/aortic">Aortic</g-link>
+        <g-link class="nav__link" to="/heart">Heart</g-link>
+        <g-link class="nav__link" to="/cardiovascular">Cardiovascular</g-link>
+        <g-link class="nav__link" to="/medical">Medical</g-link>
+      </nav>
+    </header>
+    <slot/>
+      <site-footer></site-footer>
+  </div>
+</template>
+
+
+<style>
+body {
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  margin:0;
+  padding:0;
+  line-height: 1.5;
+
+}
+
+.layout {
+  /*max-width: 760px;*/
+  /*margin: 0 auto;*/
+  /*padding-left: 20px;*/
+  /*padding-right: 20px;*/
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /*margin-bottom: 20px;*/
+  /*height: 80px;*/
+}
+
+.nav__link {
+  margin-left: 20px;
+  color: #008737;
+  font-size:16px;
+  line-height:60px;
+}
+
+.nav__link:hover {
+    border-bottom: 1px solid;
+    color: #008737;
+}
+
+
+.primaryHeaderLogoWrapperLink{
+
+    margin-left:20px !important;
+}
+
+
+
+img.primaryHeaderLogo {
+  height: 60px;
+  /*width: auto;*/
+}
+
+  header.nav{
+    float:left;
+      margin:0 20px;
+
+  }
+
+
+</style>
+<script>
+    import SiteFooter from "~/components/SiteFooter.vue";
+
+    export default {
+        components: {
+            "site-footer": SiteFooter
+        },
+        mounted() {
+        }
+    };
+</script>
